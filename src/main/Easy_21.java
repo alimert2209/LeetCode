d756497d04e8339cca28b460a4f5340ab1148b54
Ali Mert <alimert3215@gmail.com>
2026-02-28 07:21:29 +0300

    LeetCode Easy 21 (Merge Two Sorted Lists)

--- src/main/Easy_21.java
+++ src/main/Easy_21.java
@@ -11,8 +11,21 @@ public class Easy_21 {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // TODO
-        return null;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (list1 != null && list2 != null){
+            if (list1.val <= list2.val){
+                current.next = list1;
+                list1 = list1.next;
+                current = current.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+                current = current.next;
+            }
+        }
+        current.next = list1 == null ? list2 : list1;
+        return dummy.next;
     }
 
     // ---------------- TEST RUNNER ----------------
